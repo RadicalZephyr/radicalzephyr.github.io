dfa0e73c7dd9e97ef0c6d3f9cdaf2f55118445b0
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-28 12:46:25 -0400

    Fix quoting issues by getting the raw version of the command string

--- plugins/org_converter.rb
+++ plugins/org_converter.rb
@@ -30,7 +30,7 @@ module Jekyll
         puts "Could not find an emacs executable."
       else
         out, status = Open3.capture2("#{emacs_path} --batch --eval " +
-                                     "\"#{@@emacs_org_html_cmd}\"",
+                                     "#{@@emacs_org_html_cmd.inspect}",
                                      :stdin_data=>content)
         out unless status != 0
       end
