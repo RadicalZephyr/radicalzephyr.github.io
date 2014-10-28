1a4793d7dd6e572104674075d6cad2184eeb4a1c
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-28 00:19:14 -0400

    Add an org mode converter

---
+++ plugins/org_converter.rb
@@ -0,0 +1,36 @@
+# A generator that uses a emacs invoked in batch mode to process
+# org-mode files into html
+
+require 'open3'
+
+module Jekyll
+
+  class OrgModeConverter < Converter
+    safe false
+    priority :low
+
+    emacs_org_html_cmd = "(progn (require 'org) (progn (condition-case nil (while t (insert (read-string \"\") \"\\n\")) (error nil)) (set-buffer (org-export-to-buffer 'html \"*Org HTML Export*\" nil nil nil t nil (lambda () t))) (message \"%s\" (buffer-string))))"
+
+    def matches(ext)
+      ext =~ /^\.org$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      emacs_path = `which emacs`
+
+      if emacs_path.empty?
+        puts "Could not find an emacs executable."
+      else
+        out, status = Open3.capture2("#{emacs_path} --batch --eval " +
+                                     "\"#{emacs_org_html_cmd}\"",
+                                     :stdin_data=>content)
+        out unless status != 0
+      end
+
+    end
+  end
+end
