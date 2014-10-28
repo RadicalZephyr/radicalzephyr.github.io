5179abf53ac769078570a2c8f6f8b53e5d4e9305
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-28 12:48:48 -0400

    Capture both stdout and stderr
    Because apparently (message) puts to stderr...

--- plugins/org_converter.rb
+++ plugins/org_converter.rb
@@ -29,9 +29,9 @@ module Jekyll
       if emacs_path.empty?
         puts "Could not find an emacs executable."
       else
-        out, status = Open3.capture2("#{emacs_path} --batch --eval " +
-                                     "#{@@emacs_org_html_cmd.inspect}",
-                                     :stdin_data=>content)
+        out, status = Open3.capture2e("#{emacs_path} --batch --eval " +
+                                      "#{@@emacs_org_html_cmd.inspect}",
+                                      :stdin_data=>content)
         out unless status != 0
       end
 
